5018f6f546fa72bdbfca7a2e7ec820f2e48f8f01
rajuf6 <rajupanugantif6@gmail.com>
2020-06-04 23:51:27 +0530

    first commit

---
+++ raju/annaya.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+for i in range(5):
+    print('annaya di bauntadi ')
+    sleep(1.5)
+    print('enti telusa')
+
+print('hair style')    
--- translation.py
+++ translation.py
@@ -1,13 +1,3 @@
-def translate(phrase):
-    translation=''
-    for letter in phrase:
-        if letter.lower() in 'aeiou':
-            if letter.isupper():
-                translation=translation+'G'
-            else:    
-                translation=translation+'g'
-        else:
-            translation=translation+letter
-    return translation
-
-print(translate(input('Enter phrase: '))) 
+file = open('reading.txt','a')
+file.write('\nyou selected @ xilink')
+file.close()
